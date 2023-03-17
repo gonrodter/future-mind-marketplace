4218a3183fcec378ca61e613350c509b9d33eabc
Gonzalo <gonrodter@alum.us.es>
2023-03-17 22:49:03 +0100

    keep trying things for the navbar burger menu :)

--- src/components/Reutilized/Navbar.jsx
+++ src/components/Reutilized/Navbar.jsx
@@ -21,7 +21,11 @@ const Navbar = ({ accounts, setAccounts }) => {
   }
 
   return (
-    <nav className="flex fixed w-full h-auto py-4 justify-between items-start md:items-center shadow-md md:h-20 font-body bg-white z-50 px-12">
+    <nav
+      className={`${
+        isNavOpen ? "items-start" : "items-center"
+      } flex fixed w-full h-auto py-4 justify-between md:items-center shadow-md md:h-20 font-body bg-white z-50 px-12`}
+    >
       <div className="flex items-center pr-2">
         <img
           className="w-8 h-8 md:w-10 md:h-10 mr-2"
@@ -37,26 +41,20 @@ const Navbar = ({ accounts, setAccounts }) => {
       </div>
       <div>
         <FiMenu
-          className="md:hidden items-center
-           text-secondary-blue h-6 w-6 cursor-pointer float-right"
+          className={`${isNavOpen ? "mt-1" : "mt-0"} md:hidden items-center
+           text-secondary-blue h-6 w-6 cursor-pointer float-right`}
           onClick={() => setIsNavOpen(!isNavOpen)}
         />
-        <ul className={`${isNavOpen ? "block" : "hidden"} pt-8 md:flex`}>
+        <ul className={`${isNavOpen ? "block" : "hidden"} pt-10 md:pt-0 md:flex`}>
           <div className="md:flex gap-5 lg:gap-8 text-right md:text-center text-primary-blue no-underline text-base md:text-lg tracking-wider font-normal">
             <li>
-              <Link to="/marketplace">
-                Marketplace
-              </Link>
+              <Link to="/marketplace">Marketplace</Link>
             </li>
             <li>
-              <Link to="/futuremind">
-                FutureMind
-              </Link>
+              <Link to="/futuremind">FutureMind</Link>
             </li>
             <li>
-              <Link to="/">
-                About
-              </Link>
+              <Link to="/">About</Link>
             </li>
             <li>
               {isConnected ? (
